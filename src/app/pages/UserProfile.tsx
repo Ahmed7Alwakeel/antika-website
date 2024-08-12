38c29637ee660bb35d3cd21086d9e745169d8661
Ahmed7Alwakeel <alwakeel1996@gmail.com>
2024-08-12 17:16:13 +0300

    fix build issue

--- src/app/pages/UserProfile.tsx
+++ src/app/pages/UserProfile.tsx
@@ -2,12 +2,13 @@ import { useUpdateMyUser } from "../../modules/user/api/API";
 import UserProfileForm from "../../modules/user/components/UserProfileForm";
 
 const UserProfile = () => {
-  const { updateUser } = useUpdateMyUser();
+  const { updateUser, isLoading } = useUpdateMyUser();
 
 
   return (
     <UserProfileForm
       onSave={updateUser}
+      isLoading={isLoading}
     />
   );
 }
