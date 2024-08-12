69da02421cf4edf7540c38ff5a06cb3fbdab511a
Ahmed7Alwakeel <alwakeel1996@gmail.com>
2024-08-12 20:34:52 +0300

    ready for build

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import AuthCallbackPage from './pages/AuthCallbackPage';
 import UserProfile from './pages/UserProfile';
